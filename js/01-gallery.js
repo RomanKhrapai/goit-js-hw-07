62431b9cd39b04ab497c626b7536f5c99d6b436b
RomanKhrapai <hrapayr@gmail.com>
2021-10-18 17:06:49 +0300

    рішення

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('div.gallery');
+let modal;
+
+const createGalleryHTML = () => {
+    return galleryItems.map(galleryItemsToHTML).join('');
+}
+
+const galleryItemsToHTML = (item) =>
+    {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+        />
+        </a>
+        </div>`;
+    };
+
+const creatImages = (event)=>{
+    event.preventDefault();
+    if(event.target.nodeName === "IMG"){
+        modal = basicLightbox.create(`
+            <img width="1400" height="900" src="${event.path[0].dataset.source}" alt="${event.path[0].alt}">
+        `);
+        modal.show();
+    }    
+}
+const keyDounEsc = event => {
+    if(event.key==="Escape" && basicLightbox.visible()){
+        modal.close();
+    };
+  }
+
+gallery.insertAdjacentHTML('beforeend',createGalleryHTML());
+gallery.addEventListener('click',creatImages);
+document.addEventListener("keydown", keyDounEsc);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryUl = document.querySelector('ul.gallery');
+
+const createGalleryHTML = () => {
+    return galleryItems.map(galleryItemsToHTML).join('');
+}
+
+const galleryItemsToHTML = (item) =>
+    {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+        />
+        </a>
+        </li>`;
+    };
+   
+    galleryUl.insertAdjacentHTML('beforeend',createGalleryHTML());
+
+let gallery = new SimpleLightbox('.gallery a',{captionDelay:250,captionsData:'alt'});
+
+
+
+
